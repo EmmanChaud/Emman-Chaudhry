f19f3fd5d3f1523535625ba04ccd87e091360353
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-11-24 11:41:07 +0000

    Create a branch to implement the StrStack class which is used as a facade

---
+++ Assignment2/branches/TestStrStack/src/mainCodes/NumStack.java
@@ -0,0 +1,35 @@
+package mainCodes;
+
+import mainCode.BadTypeException;
+import mainCode.EmptyStackException;
+import mainCode.Entry;
+import mainCode.Stack;
+
+public class NumStack {
+	
+	Stack numStack = new Stack(); 
+	Entry entry = new Entry();
+	private float size = 0;
+
+	public void push(float f) {	
+		numStack.push(new Entry(1f));
+		size += 1;
+	}
+
+	public Boolean isEmpty() {
+		return numStack.isEmpty();
+	}
+
+	public float pop() throws BadTypeException, EmptyStackException {
+		if (!isEmpty()) {
+			size --;
+			return numStack.pop().getValue();
+		}
+		throw new EmptyStackException();
+	}
+
+	public float size() {
+		return size;
+	}
+
+}
