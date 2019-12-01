ae19c33f2709f9e5f6fc43a0942234a88e8ab54c
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-01 22:27:58 +0000

    Create a branch for the RevPolishCalc jUnit test and StandardCalc jUnit test

---
+++ CS2800/Assignment2/branches/TestCalculatorBranch/testCodes/TestStrStack.java
@@ -0,0 +1,40 @@
+package testCodes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.junit.jupiter.api.Test;
+
+import mainCode.BadTypeException;
+import mainCode.EmptyStackException;
+import mainCodes.StrStack;
+
+class TestStrStack {
+
+
+	/**
+	 * TEST1: testing to push a String onto the Stack strStack 
+	 * @throws BadTypeException 
+	 */
+	@Test
+	public void testPush() throws BadTypeException {
+		StrStack tester = new StrStack();
+		tester.push("a");
+		assertFalse(tester.isEmpty());
+	}
+	
+	/**
+	 * TEST2: test to remove an String from the Stack strStack
+	 * @throws EmptyStackException
+	 * @throws BadTypeException
+	 */
+	@Test
+	public void testPop() throws EmptyStackException, BadTypeException {
+		StrStack tester = new StrStack();
+		tester.push("a");
+		tester.push("b");
+		tester.push("c");
+		tester.pop();
+		assertEquals("b", tester.getString().toString());
+		}
+
+}
