3949eb37bf40fc0906b0e28a441add14db8a07b3
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-06 18:18:57 +0000

    'Add' class for the interpreter design pattern

---
+++ CS2800/Assignment2/branches/CalculatorBranch/mainCodes/Add.java
@@ -0,0 +1,15 @@
+package mainCodes;
+
+public class Add {
+
+	public void add(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean evaluate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
