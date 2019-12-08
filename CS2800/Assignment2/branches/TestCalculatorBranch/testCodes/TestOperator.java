1259b14c86d5f3a6b058e2f478b8ae45cc93076b
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-08 21:51:18 +0000

    Deleted TestAdd class since it wasn't necessary Refactored other test classes

--- CS2800/Assignment2/branches/TestCalculatorBranch/testCodes/TestOperator.java
+++ CS2800/Assignment2/branches/TestCalculatorBranch/testCodes/TestOperator.java
@@ -1,10 +1,17 @@
 package testCodes;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import mainCodes.Add;
+import mainCodes.Calculator;
+import mainCodes.Divide;
+import mainCodes.NumberExp;
 import mainCodes.Operator;
+import mainCodes.Subtract;
+import mainCodes.Times;
 
 class TestOperator {
 
@@ -13,6 +20,20 @@ class TestOperator {
 		Operator tester = new Operator();
 		String operand = "+";
 		assertTrue("+", tester.isOperator(operand));
+	}
+	
+	@Test
+	void test2() {
+		Operator tester = new Operator();
+		String operand = "+";
+		Calculator left = null;
+		Calculator right = null;
+		Add add = new Add(left,right);
+		Subtract subtract = new Subtract(left,right);
+		Times times = new Times(left, right);
+		Divide divide = new Divide(left,right);
+		NumberExp number = new NumberExp(1f);
+		assertNotNull(tester.getOperator(operand, left, right));
 	}
 
 }
--- CS2800/Assignment2/branches/TestCalculatorBranch/testCodes/TestRevPolishCalc.java
+++ CS2800/Assignment2/branches/TestCalculatorBranch/testCodes/TestRevPolishCalc.java
@@ -1,6 +1,5 @@
 package testCodes;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -11,12 +10,13 @@ import mainCodes.RevPolishCalc;
 
 class TestRevPolishCalc {
 
+	
 	@Test
-	void test() throws BadTypeException, EmptyStackException {
+	void test2() throws BadTypeException, EmptyStackException {
 		RevPolishCalc tester = new RevPolishCalc();
-		String a = "1f";
-		tester.evaluate(a);
-		assertEquals(1f, tester.getValue());
+		String string = "5 6 7 + * 2 -";
+		assertEquals(63, tester.evalExpression(string));
+		System.out.println(tester.evalExpression(string)); 
 	}
 
 }
