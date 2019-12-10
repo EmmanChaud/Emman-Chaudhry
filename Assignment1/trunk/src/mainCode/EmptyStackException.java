71d6c7e8753e8f3f1f48f477a7827adde28cdab5
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-10 22:16:59 +0000

    Updated classes in Assignment1, in particular the Symbol class

---
+++ Assignment1/trunk/src/mainCode/EmptyStackException.java
@@ -0,0 +1,5 @@
+package mainCode;
+
+public class EmptyStackException extends Exception {
+
+}
---
+++ Assignment1/trunk/src/mainCode/Type.java
@@ -0,0 +1,11 @@
+package mainCode;
+
+/**
+ * enum class to represent all Type names
+ * 
+ * @author Emman
+ *
+ */
+public enum Type {
+  NUMBER, STRING, INVALID, SYMBOL;
+}
