ccd48d241a4ec834f54c3adf9f4edb6ab3c78876
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-12 21:18:49 +0000

    Create a branch to edit and refine existing code in the controller and calculator branch

---
+++ CS2800/CalculatorGraphics/branches/ChangeControllerAndCalculatorBranch/src/application/ViewInterface.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.function.Consumer;
+
+public interface ViewInterface {
+
+  void addCalcObserver(Observer f);
+
+  void addTypeObserver(Consumer<OpType> l);
+
+  String getQuestion();
+
+  void setAnswer(String a);
+  ///////////////////////////////////////////////////////////////////
+
+}
