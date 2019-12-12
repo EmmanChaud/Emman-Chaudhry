fe61dc594880f189cbe4960e6253e14b4c24b36b
zfac213 <zfac213@f6831ffe-1fb1-4cb9-8f24-d601722222fb>
2019-12-12 20:02:04 +0000

    Checked out the folder when it wasn't a working copy

--- CS2800/CalculatorFX/trunk/src/application/Observer.java
+++
@@ -1,6 +0,0 @@
-package application;
-
-@FunctionalInterface
-public interface Observer {
-	public void tell();
-}
--- CS2800/CalculatorFX/trunk/src/application/OpType.java
+++
@@ -1,5 +0,0 @@
-package application;
-
-public enum OpType {
-  INFIX, POSTFIX;
-}
